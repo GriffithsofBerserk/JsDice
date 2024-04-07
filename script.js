e0a8c31414bd8c50581235746513d00a275016a3
Suat Can Ölçer <132690977+GriffithsofBerserk@users.noreply.github.com>
2024-04-07 04:54:46 +0300

    added dices and changed definitions

---
+++ script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const player0EL = document.querySelector('.player--0');
+
+const player1EL = document.querySelector('.player--1');
+
+const score0EL = document.querySelector('#score--0');
+
+const score1EL = document.querySelector('#score--1');
+
+const current0EL = document.getElementById('current--0');
+
+const current1EL = document.getElementById('current--1');
+
+const diceEL = document.querySelector('.dice');
+
+const btnNew = document.querySelector('.btn--new');
+
+const btnRoll = document.querySelector('.btn--roll');
+
+const btnHold = document.querySelector('.btn--hold');
+
+let scores, currentScore, activePlayer, playing;
+
+const init = function(){
+    scores[0,0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0EL.textContent = 0;
+    score1EL.textContent = 0;
+    current0EL.textContent = 0;
+    current1EL.textContent = 0;
+
+    diceEL.classList.add('hidden');
+    player0EL.classList.remove('player--active');
+    player1EL.classList.remove('player--active');
+}
+
